6ea4990571c95a109a90722259f93d89b0fc57bf
Thomas Jensen <thomas@jensen.holdings>
2025-07-05 22:42:29 +0200

    Refactor

--- test.js
+++ test.js
@@ -1,23 +1,21 @@
 import test from 'tape'
 import join from './index.js'
 
-test((t) => {
-	t.plan(1)
+test(async (t) => {
 	const a = Promise.resolve(1)
 	const b = Promise.resolve(2)
 	const c = Promise.resolve(4)
-	join(a, b, c, (...args) => t.deepEqual(args, [1, 2, 4]))
+	await join(a, b, c, (...args) => t.deepEqual(args, [1, 2, 4]))
 })
 
-test('a promise is returned', (t) => {
-	const p = join(1, (...args) => {})
+test('a promise is returned', async (t) => {
+	const p = join(1, () => {})
 	t.ok(p instanceof Promise)
-	t.end()
+	await p
 })
 
-test('a promise is returned', (t) => {
-	t.plan(1)
-	join(1, () => 2).then((r) => t.equal(r, 2))
+test('a promise is returned', async (t) => {
+	await join(1, () => 2).then((r) => t.equal(r, 2))
 })
 
 test('no arguments', async (t) => {
@@ -40,41 +38,37 @@ test('one function argument', async (t) => {
 	)
 })
 
-test('mixed value types (promise and not)', (t) => {
-	t.plan(1)
-	join(Promise.resolve(1), 2, (...args) => t.deepEqual(args, [1, 2]))
+test('mixed value types (promise and not)', async (t) => {
+	await join(Promise.resolve(1), 2, (...args) => t.deepEqual(args, [1, 2]))
 })
 
-test('mixed value types (settled and unsettled)', (t) => {
-	t.plan(1)
-	const a = Promise.resolve(1)
-	let rs
-	const b = new Promise((_rs) => (rs = _rs))
-	join(a, b, (...args) => t.deepEqual(args, [1, 2]))
-	rs(2)
+test('mixed value types (settled and unsettled)', async (t) => {
+	const {promise: a, resolve: resolveA} = Promise.withResolvers()
+	const {promise: b, resolve: resolveB} = Promise.withResolvers()
+	resolveA(1)
+	const args = join(a, b, (...args) => args)
+	resolveB(2)
+	t.deepEqual(await args, [1, 2])
 })
 
-test('single rejection', (t) => {
-	t.plan(1)
+test('single rejection', async (t) => {
 	const err = new Error()
-	join(Promise.reject(err), () => t.fail()).catch((_err) =>
+	await join(Promise.reject(err), () => t.fail()).catch((_err) =>
 		t.equal(_err, err),
 	)
 })
 
-test('multiple rejections', (t) => {
-	t.plan(1)
+test('multiple rejections', async (t) => {
 	const errA = new Error()
 	const errB = new Error()
-	join(Promise.reject(errA), Promise.reject(errB), () => t.fail()).catch(
-		(_err) => t.equal(_err, errA),
-	)
+	await join(Promise.reject(errA), Promise.reject(errB), () =>
+		t.fail(),
+	).catch((_err) => t.equal(_err, errA))
 })
 
-test('both resolved and rejected', (t) => {
-	t.plan(1)
+test('both resolved and rejected', async (t) => {
 	const err = new Error()
-	join(Promise.resolve(1), Promise.reject(err), () => t.fail()).catch(
+	await join(Promise.resolve(1), Promise.reject(err), () => t.fail()).catch(
 		(_err) => t.equal(_err, err),
 	)
 })
